e7e671539e769f8abd3c9f686c9afe976e8305b5
razorrahul123 <rahul.kr.sushant@gmail.com>
2017-01-11 17:44:19 +0530

    Added Album component and did the cleanup

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,14 @@ import {SearchComponent} from './components/search/search.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {AboutComponent} from './components/about/about.component';
 import  {routing} from './app.routing';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {ArtistComponent} from "./components/artist/artist.component";
+import {AlbumComponent} from "./components/album/album.component";
 
 @NgModule({
-  imports:      [ BrowserModule, routing ],
-  declarations: [ AppComponent, NavbarComponent, SearchComponent, AboutComponent ],
+  imports:      [ BrowserModule, routing, FormsModule, HttpModule ],
+  declarations: [ AppComponent, NavbarComponent, SearchComponent, AboutComponent, ArtistComponent, AlbumComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
